310ac2e8a1484f775a324b104dfb3db28b036b8f
Raiden <wow-sk@ua.fm>
2011-01-17 19:03:57 +0200

    [19] Script/Command: Implement command for transfer item.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -61,6 +61,7 @@ void AddSC_reload_commandscript();
 void AddSC_tele_commandscript();
 void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
+void AddSC_pl_transfer_commandscript();
 
 #ifdef SCRIPTS
 //world
@@ -649,6 +650,7 @@ void AddCommandScripts()
     AddSC_tele_commandscript();
     AddSC_titles_commandscript();
     AddSC_wp_commandscript();
+    AddSC_pl_transfer_commandscript();
 }
 
 void AddWorldScripts()
